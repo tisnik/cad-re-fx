4bc82278dee85b3fa06c958f35adde6af02204be
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-08 17:14:18 +0200

    Common math functions and methods

---
+++ WEB-INF/classes/cz/tisnik/cadgfx/utils/MathCommon.java
@@ -0,0 +1,14 @@
+package cz.tisnik.cadgfx.utils;
+
+public class MathCommon
+{
+    public static int double2int(double value)
+    {
+        return (int)Math.round(value);
+    }
+
+    public static int doubleStr2int(String value)
+    {
+        return double2int(Double.parseDouble(value.trim()));
+    }
+}
